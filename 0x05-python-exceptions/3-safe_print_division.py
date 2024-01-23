78f0b1406e26a37781433e0d5256922c2905a43e
Charles <charlesigbe0@gmail.com>
2024-01-23 02:38:39 +0000

    safe print divv fixx

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,6 +8,6 @@ def safe_print_division(a,b):
         div = None
 
     finally:
-        print('Inside result: {}'.format(result))
+        print('Inside result: {}'.format(div))
 
-    return (result)
+    return (div)
