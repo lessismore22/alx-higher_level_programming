19710b69edc8accb2f3a86fbd1654cd49f376700
Charles <charlesigbe0@gmail.com>
2024-02-14 02:37:34 +0000

    first fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """This module defines a file-appending function."""
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file"""
+    """Appends a string to the end of a UTF8 text file
+    """
     with open(filename, "a", encoding="utf-8")  as f:
         return f.write(text)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,5 +4,6 @@
 import json
 
 def from_json_string(my_str):
-    """Returns the Python object representation of a JSON string"""
+    """Returns the Python object representation of a JSON string
+    """
     return json.loads(my_str)
