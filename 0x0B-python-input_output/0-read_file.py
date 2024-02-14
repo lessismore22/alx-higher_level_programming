ccccf62a38c604eeb7537c87c4584350db9b8360
Charles <charlesigbe0@gmail.com>
2024-02-14 02:30:17 +0000

    input output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+ #!/usr/bin/python3
+"""This module defines a text file-reading function"""
+
+
+def read_file(filename=""):
+    """Prints the contents of a UTF8 text file"""
+    print(f.read(), end="")
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a JSON file-reading funcion"""
+import json
+
+def load_from_json_file(filename):
+    """Creates a python object from a given JSON file"""
+    with open(filename) as f:
+        return json.load(f)
