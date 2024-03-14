c30e46d3bb5fa3f37264704070c8783de436b524
Charles <charlesigbe0@gmail.com>
2024-03-14 16:23:22 +0000

    fix 1

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,5 @@
--this script that lists all the tables of a database in your MySQL server
+- lists all the tables of a database in your MySQL server
 -The database name will be passed as argument of mysql command
 
-SHOW TABLES;
+USE mysql;
+SHOW TABLE;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,5 @@
 -This script creates a table called first_table in the current database in your MySQL server
 - The database name will be passed as an argument of the mysql command
 
-CREATE first_table IF NOT EXISTS{
-id INT
-name VARCHAR(256)
-}
+CREATE first_table IF NOT EXISTS(id INT, name VARCHAR(256))
 
