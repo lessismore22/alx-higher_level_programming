ca2301599e110362f4e9daf5094436199dbb9ded
lessismore22 <charlesigbe@gmail.com>
2024-05-22 04:44:06 +0000

    6-completed_tasks fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,40 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
 
-request.get(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const apiUrl = process.argv[2];
 
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
-      }
+if (!apiUrl) {
+    console.error('Please provide the API URL as the first argument.');
+    process.exit(1);
+}
+
+request(apiUrl, { json: true }, (error, response, body) => {
+    if (error) {
+        console.error('Error making request:', error);
+        return;
     }
-  });
-  console.log(tasksCompleted);
+
+    if (response.statusCode !== 200) {
+        console.error('Error: Received status code', response.statusCode);
+        return;
+    }
+
+    const completedTasksByUser = {};
+
+    body.forEach(task => {
+        if (task.completed) {
+            if (!completedTasksByUser[task.userId]) {
+                completedTasksByUser[task.userId] = 0;
+            }
+            completedTasksByUser[task.userId]++;
+        }
+    });
+
+    for (const userId in completedTasksByUser) {
+        if (completedTasksByUser.hasOwnProperty(userId)) {
+            console.log(`User ${userId} has completed ${completedTasksByUser[userId]} tasks`);
+        }
+    }
 });
+
